db7b16322c3163ebd980adba6093e940ed993f28
Hanseok Kim <50775570+hasuikhs@users.noreply.github.com>
2019-06-10 22:56:30 +0900

    JDBC 설명

--- 3_JDBC/src/DAO/BookDAO.java
+++ 3_JDBC/src/DAO/BookDAO.java
@@ -12,11 +12,11 @@ import vo.BookVO;
 public class BookDAO {
 	public List<BookVO> bookList() { // DB상의 Book객체들을 호출하여 booklist에 집어넣어 배열로서 호출
 		List<BookVO> list = new ArrayList<BookVO>();
-		String sql = "select * from book";
+		String sql = "select * from book"; // 북 테이블에 있는 모든 정보를 호출
 
-		Connection con = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
+		Connection con = null; // DB연결도니 상태(세션)을 담은 객체
+		PreparedStatement ps = null; // sql문을 나타내는 객체
+		ResultSet rs = null; // 쿼리문을 날린것에 대한 반환값을 담을 객체
 
 		try {
 			con = JDBCUtil.getConnection();
@@ -24,11 +24,11 @@ public class BookDAO {
 			rs = ps.executeQuery();
 
 			while (rs.next()) {
-				BookVO bookVO = new BookVO();
-				bookVO.setTitle(rs.getString("title"));
-				bookVO.setPrice(rs.getInt("price"));
+				BookVO bookVO = new BookVO(); // 새로운 책 객체 생성
+				bookVO.setTitle(rs.getString("title")); // 만들어진 책 객체에 쿼리문에 대한 반환값중 title을 담음
+				bookVO.setPrice(rs.getInt("price")); // 마찬가지로 price를 담음
 				// 필요시 추가 로드 작업
-				list.add(bookVO);
+				list.add(bookVO); // 만들어진 북 객체를 list에 
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
